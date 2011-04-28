5db10620256b1bc134104e0b0191cc3c92047cbf
Cody Krieger <cody.krieger@gmail.com>
2011-04-28 15:07:50 -0400

    this should be in an individual app, not the gem

--- app/controllers/blue_sparks/pages_controller.rb
+++ app/controllers/blue_sparks/pages_controller.rb
@@ -20,24 +20,6 @@ class BlueSparks::PagesController < ApplicationController
     end
   end
 
-  def subnav
-    # magic method to render a given folder's subnav partial
-    begin
-      subnav = render :partial => "#{current_page}/menu", :layout => "menu_template"
-      subnav.first unless subnav.nil?
-    rescue
-      logger.info "subnav error: #{$!}"
-
-      begin
-        subnav = render :partial => "#{page_root}/menu", :layout => "menu_template"
-      rescue
-        logger.info "subnav error: #{$!}"
-      end
-    end
-    logger.info "#{subnav.inspect}"
-    subnav.first unless subnav.nil?
-  end
-
 protected
 
   def current_page
